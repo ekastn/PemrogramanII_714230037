93ade4d99413869777aa4755327f9ee325413979
ekastn <118880703+ekastn@users.noreply.github.com>
2024-10-15 20:37:32 +0700

    fix: menyesuaikan kode karena salah membaca soal

--- Pertemuan02/Praktikum/P2_1_714230037/Program.cs
+++ Pertemuan02/Praktikum/P2_1_714230037/Program.cs
@@ -4,8 +4,8 @@ Console.Write("b: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"""
-a + b = {a + b}
-a - b = {a - b}
-a x b = {a * b}
-a / b = {a / b}
+{a} + {b} = {a + b}
+{a} - {b} = {a - b}
+{a} x {b} = {a * b}
+{a} / {b} = {a / b}
 """);
--- Pertemuan02/Praktikum/P2_1_714230037/obj/Debug/net8.0/P2_1_714230037.AssemblyInfo.cs
+++ Pertemuan02/Praktikum/P2_1_714230037/obj/Debug/net8.0/P2_1_714230037.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P2_1_714230037")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a424be2eed8e7453f089b81ec3b7675e05543c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("P2_1_714230037")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P2_1_714230037")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
