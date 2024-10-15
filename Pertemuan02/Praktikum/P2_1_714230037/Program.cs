7a424be2eed8e7453f089b81ec3b7675e05543c4
ekastn <118880703+ekastn@users.noreply.github.com>
2024-10-15 16:09:58 +0700

    pertemuan 2

---
+++ Pertemuan02/Praktikum/P2_1_714230037/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"""
+a + b = {a + b}
+a - b = {a - b}
+a x b = {a * b}
+a / b = {a / b}
+""");
