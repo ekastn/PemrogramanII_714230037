6dc98dccc7a070763bb18a188ea827193816c6f2
ekastn <118880703+ekastn@users.noreply.github.com>
2024-11-09 01:02:27 +0700

    Praktikum 05

---
+++ Pertemuan05/Praktikum/P5_1_714230037/P5_1_714230037/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace P5_1_714230037
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void HelloButton_Click(object sender, EventArgs e)
+        {
+            outputLabel.Text = "Hello World";
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Pertemuan05/Praktikum/P5_3_714230037/P5_3_714230037/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_3_714230037
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTampilkan_Click(object sender, EventArgs e)
+        {
+            string os = "";
+            string status = "";
+            if(rb_android.Checked == true)
+            {
+                os = "Android";
+            }
+            else if(rb_ios.Checked == true){
+                os = "IOS";
+            }
+            if (cbYa.Checked == true)
+            {
+                status = "Sudah Diperbaiki";
+            }
+            else
+            {
+                status = "Belum di Perbaiki";
+            }
+            MessageBox.Show(
+                "Merk HP: " + txtMerkHp.Text + "\nSistem Operasi: " + os + "\nStatus Perbaikan: " + status ,
+                "Informasi Service HP",
+                MessageBoxButtons.OK, MessageBoxIcon.Information); 
+        }
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
